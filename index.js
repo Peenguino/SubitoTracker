e29c2be764beca638bf6c48a99673c3b25fe7265
peenguino <giuseppe_acocella@outlook.it>
2024-06-06 00:29:14 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const https = require("https")
 const telegramBot = require("node-telegram-bot-api")
+const { resolve } = require("path")
+
+//var resObj
 
 //token bot telegram
 class NoToken extends Error{}
@@ -12,35 +15,81 @@ if(!(telegramToken))
 
 // ======== gestione asincrona del tracking 
 
+async function getSubitoJSON()
+{
+    //possibile variazione codice, passando url come parametro
+    let url = "https://hades.subito.it/v1/search/items?c=43&r=15&ci=5&to=065052&t=u&qso=false&shp=false&urg=false&sort=datedesc&lim=30&start=0"
+    return new Promise((resolve,reject) =>
+    {
+        https.get(url,(res) => {
 
+            let body = "";
+        
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+        
+            res.on("end", () => {
+                try {
+                    resolve(JSON.parse(body))
+    
+                } catch (error) {
+                    reject(error);
+                };
+            });
+        
+        })
+    })
+}
+
+async function keepTracking()
+{
+    let i = 0
+    // /*
+    let adsList = await getSubitoJSON()
+    let checkNewObj = adsList.checknew
+    // */
+    while(true)
+    {   
+        //console.log("Pippo")
+        let adsList = await getSubitoJSON()
+        let checkNewObj = adsList.checknew
+        setTimeout(async ()=>
+        {
+            if(checkNewObj.newads == true)
+            {
+                bot.sendMessage(chatId,"Nuovo annuncio pubblicato")
+                adsList = await getSubitoJSON()
+                checkNewObj = adsList.checknew
+                console.log("pippo")
+            }
+            else
+            {
+                bot.sendMessage(chatId,"Nessun annuncio pubblicato")
+            }
+        },10000)//600000)
+    }
+}
 
 // ========
 
 let bot = new telegramBot(telegramToken,{polling:true})
 
-bot.onText(/[/]{1}track/,(msg,match)=>{
+bot.onText(/[/]{1}track/,async (msg,match)=>{
     chatId = msg.chat.id
-    let url = "https://hades.subito.it/v1/search/items?c=43&r=15&ci=5&to=065052&t=u&qso=false&shp=false&urg=false&sort=datedesc&lim=30&start=0"
-    https.get(url,(res) => {
-        let body = "";
-    
-        res.on("data", (chunk) => {
-            body += chunk;
-        });
-    
-        res.on("end", () => {
-            try {
-                let obj = JSON.parse(body);
-                
-                console.log(obj.ads[0])
-
-            } catch (error) {
-                console.error(error.message);
-            };
-        });
-    
-    }).on("error", (error) => {
-        console.error(error.message);
-    });
-
+    await keepTracking()
 })
+
+
+bot.onText(/[/]{1}debug/,(msg,match)=>{
+    chatId = msg.chat.id
+    while(true)
+    {
+        setTimeout(()=>
+        {
+            console.log("pippo")
+        },10000)//600000)
+    }
+})
+
+
